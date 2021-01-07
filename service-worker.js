868fd52171e50195d7f7dbcfeb4f192a866c878c
swim-mail <milkcoffee@kakao.com>
2021-01-07 14:36:51 +0900

    pwa lighthouse 조건 만족

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,39 @@
 //service-worker.js
-const CACHE_NAME = 'swim-mail-cache'; 
+const CACHE_NAME = "swim-mail-cache";
 
 const FILES_TO_CACHE = [
-    '/icons/ico32.png',
-    '/icons/ico512.png',
-    '/icons/fav16.png',
-    '/icons/fav32.png',
-    '/app.webmanifest',
-    '/css/style-min.css',
-    '/js/main-min.js',
-    '/index.html',
+  "/icons/ico32.png",
+  "/icons/ico512.png",
+  "/icons/fav16.png",
+  "/icons/fav32.png",
+  "/app.webmanifest",
+  "/css/style-min.css",
+  "/js/main-min.js",
+  "/index.html",
+  "/",
 ];
 
-self.addEventListener('install', (event) => {
-
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
-
-self.addEventListener('fetch', (event) => {
-    
-    if (event.request.method !== 'GET') { // GET 요청만 캐싱 지원 처리
-        return;
-    }
-
-    const fetchRequest = event.request.clone();
-
-    event.respondWith(
-        fetch(fetchRequest)
-            .then((response) => {
-                caches.open(CACHE_NAME) // 네트워크 요청 성공시 해당 결과값 캐싱
-                      .then(cache => cache.put(event.request.url, response.clone()));
-                return response;
-            })
-            .catch(() => {
-                return caches.match(event.request.url)
-                    .then(cache => {return cache;}) // 네트워크 요청 실패시 캐싱된 요청으로 응답.
-            })
-    )
-    ;
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(CACHE_NAME).then(function (cache) {
+            console.log(
+              "[Service Worker] Caching new resource: " + e.request.url
+            );
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
 });
